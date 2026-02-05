232fd6a2694ecf837ede1bdb98ee8b66d6b6ec6f
Linus <kramerl@ethz.ch>
2026-02-05 20:00:17 +0100

    Update launch file from b2w_rsl construction branch

--- launch/dlio.launch.py
+++ launch/dlio.launch.py
@@ -1,94 +1,73 @@
-#
-#   Copyright (c)     
-#
-#   The Verifiable & Control-Theoretic Robotics (VECTR) Lab
-#   University of California, Los Angeles
-#
-#   Authors: Kenny J. Chen, Ryan Nemiroff, Brett T. Lopez
-#   Contact: {kennyjchen, ryguyn, btlopez}@ucla.edu
-#
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition   
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch_ros.substitutions import FindPackageShare
+from launch.actions import RegisterEventHandler, ExecuteProcess
+from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
-    current_pkg = FindPackageShare('direct_lidar_inertial_odometry')
+    current_pkg = FindPackageShare("direct_lidar_inertial_odometry")
 
-    # Set default arguments
-    rviz = LaunchConfiguration('rviz', default='false')
-    pointcloud_topic = LaunchConfiguration('pointcloud_topic', default='points_raw')
-    imu_topic = LaunchConfiguration('imu_topic', default='imu_raw')
+    # Args
+    pointcloud_topic = LaunchConfiguration('pointcloud_topic', default='/lidar/point_cloud')
+    imu_topic = LaunchConfiguration('imu_topic', default='/imu_sensor_broadcaster/imu')
 
-    # Define arguments
-    declare_rviz_arg = DeclareLaunchArgument(
-        'rviz',
-        default_value=rviz,
-        description='Launch RViz'
-    )
     declare_pointcloud_topic_arg = DeclareLaunchArgument(
         'pointcloud_topic',
-        default_value=pointcloud_topic,
+        default_value='/lidar/point_cloud',
         description='Pointcloud topic name'
     )
     declare_imu_topic_arg = DeclareLaunchArgument(
         'imu_topic',
-        default_value=imu_topic,
+        default_value='/imu_sensor_broadcaster/imu',
         description='IMU topic name'
     )
 
-    # Load parameters
-    dlio_yaml_path = PathJoinSubstitution([current_pkg, 'cfg', 'dlio.yaml'])
-    dlio_params_yaml_path = PathJoinSubstitution([current_pkg, 'cfg', 'params.yaml'])
+    # Load DLIO parameters
+    dlio_yaml_path = PathJoinSubstitution([current_pkg, "cfg", "dlio.yaml"])
+    dlio_params_yaml_path = PathJoinSubstitution([current_pkg, "cfg", "params.yaml"])
 
     # DLIO Odometry Node
     dlio_odom_node = Node(
-        package='direct_lidar_inertial_odometry',
-        executable='dlio_odom_node',
-        output='screen',
+        package="direct_lidar_inertial_odometry",
+        executable="dlio_odom_node",
+        output="screen",
         parameters=[dlio_yaml_path, dlio_params_yaml_path],
         remappings=[
-            ('pointcloud', pointcloud_topic),
-            ('imu', imu_topic),
-            ('odom', 'dlio/odom_node/odom'),
-            ('pose', 'dlio/odom_node/pose'),
-            ('path', 'dlio/odom_node/path'),
-            ('kf_pose', 'dlio/odom_node/keyframes'),
-            ('kf_cloud', 'dlio/odom_node/pointcloud/keyframe'),
-            ('deskewed', 'dlio/odom_node/pointcloud/deskewed'),
+            ("pointcloud", pointcloud_topic),
+            ("imu", imu_topic),
+            ('map_pose', 'dlio/odom_node/map_pose'),
+            ("odom", "dlio/odom_node/odom"),
+            ("pose", "dlio/odom_node/pose"),
+            ("path", "dlio/odom_node/path"),
+            ("kf_pose", "dlio/odom_node/keyframes"),
+            ("kf_cloud", "dlio/odom_node/pointcloud/keyframe"),
+            ("deskewed", "dlio/odom_node/pointcloud/deskewed"),
+            ('deskewed_not_transformed', 'dlio/odom_node/pointcloud/deskewed_not_transformed'),
         ],
+        respawn=True,
     )
 
     # DLIO Mapping Node
     dlio_map_node = Node(
-        package='direct_lidar_inertial_odometry',
-        executable='dlio_map_node',
-        output='screen',
+        package="direct_lidar_inertial_odometry",
+        executable="dlio_map_node",
+        output="screen",
         parameters=[dlio_yaml_path, dlio_params_yaml_path],
         remappings=[
-            ('keyframes', 'dlio/odom_node/pointcloud/keyframe'),
+            ('kf_cloud', 'dlio/odom_node/pointcloud/keyframe'),
+            ('map_pose', 'dlio/odom_node/map_pose'),
         ],
-    )
-
-    # RViz node
-    rviz_config_path = PathJoinSubstitution([current_pkg, 'launch', 'dlio.rviz'])
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='dlio_rviz',
-        arguments=['-d', rviz_config_path],
-        output='screen',
-        condition=IfCondition(LaunchConfiguration('rviz'))
+        respawn=True,
     )
 
     return LaunchDescription([
-        declare_rviz_arg,
-        declare_pointcloud_topic_arg,
-        declare_imu_topic_arg,
+        # 1) Nodes
         dlio_odom_node,
         dlio_map_node,
-        rviz_node
+        # 2) Arguments
+        declare_pointcloud_topic_arg,
+        declare_imu_topic_arg,
     ])
